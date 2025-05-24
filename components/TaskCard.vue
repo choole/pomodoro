c35cba373dca66a07d22674a5e64d63d421fec9d
choole <nemanjacukic31@gmail.com>
2025-05-24 14:23:52 +0200

    Add: add_task button, delete button and inplace element for editing task name

--- components/TaskCard.vue
+++ components/TaskCard.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="flex flex-grow items-center justify-center">
-    <Checkbox v-model="task" inputId="task1" name="task" :value="label" class="mr-7"/>
-    <label for="task1">{{label}}</label>
-    <Button icon="pi pi-trash" severity="danger" raised aria-label="Delete" class="ml-auto"/>
+      <Checkbox :input-id="task.code" :name="task.name" :value="task.name" class="mr-7"/>
+     <Inplace>
+            <template #display>
+                {{ task.name || 'Click to Edit' }}
+            </template>
+            <template #content="{ closeCallback }">
+                <span class="inline-flex items-center gap-2">
+                    <InputText v-model="task.name" autofocus @change="update"/>
+                    <Button icon="pi pi-times" text severity="danger" @click="closeCallback" />
+                </span>
+            </template>
+        </Inplace>
+      <!-- <label :for="task.code">{{task.name}}</label> -->
+    <Button icon="pi pi-trash" severity="danger" raised aria-label="Delete" class="ml-auto" :style="{ visibility: hide }" @click="emit('delete', task.code)" />
     </div>
 </template>
 
 <script setup>
 import Checkbox from 'primevue/checkbox';
 import Button from 'primevue/button';
-const task = ref('');
+import InputText from 'primevue/inputtext';
+import { defineProps, defineEmits } from 'vue'
 defineProps({
-  label: String
-})
+  task: Object,
+  hide: String,
+});
+const emit = defineEmits(['delete','update-task']);
+function update(taskCode,taskName){
+  const task = { name: taskName, code: taskCode };
+  emit('update-task',task);
+}
 </script>
--- components/TaskList.vue
+++ components/TaskList.vue
@@ -3,38 +3,64 @@
     <div class="card flex justify-center pt-30">
          <ul class="w-full items-center border rounded-md divide-y divide-gray-200">
               <li
-                v-for="item in countries"
+                v-for="item in tasks"
                 :key="item.code"
                 :class="[
                     'p-3 cursor-pointer hover:bg-gray-700',
                 ]"
+                @mouseover="showDelete(item.code)"
+                @mouseleave="hideDelete()"
               >
                 <div class="flex items-center">
-                    <TaskCard :label="item.name"/>
+                    <TaskCard 
+                        :task="item"
+                        :hide="hoveredItem === item.code ? 'visible': 'hidden' "
+                        @delete="removeTask(item.code)"
+                        @update-task="handleUpdateTask(item)"
+                    />
                 </div>
             </li>
         </ul>
     </div>
 <!-- Add Button -->
+    <div>
+        <Button icon="pi pi-plus" label="Click to add a task here" severity="primary" raised variant="outlined" aria-label="Add here the task that you will focus on" class="w-full mt-5" @click="addTask" />
+    </div>
 </template>
 
 <script setup>
 
 import { ref } from "vue";
-import Listbox from 'primevue/listbox';
 import TaskCard from "./TaskCard.vue";
 
-const selectedItem = ref();
-const countries = ref([
-    { name: 'Australia', code: 'AU' },
-    { name: 'Brazil', code: 'BR' },
-    { name: 'China', code: 'CN' },
-    { name: 'Egypt', code: 'EG' },
-    { name: 'France', code: 'FR' },
-    { name: 'Germany', code: 'DE' },
-    { name: 'India', code: 'IN' },
-    { name: 'Japan', code: 'JP' },
-    { name: 'Spain', code: 'ES' },
-    { name: 'United States', code: 'US' }
-]);
+const hoveredItem = ref(null);
+const tasks=ref([]);
+
+
+function addTask() {
+    tasks.value.push(
+        {
+            name: '',
+            code: Date.now()
+        }
+    );
+}
+function removeTask(taskCode) {
+  const index = tasks.value.findIndex(t => t.code === taskCode)
+  if (index !== -1) {
+    tasks.value.splice(index, 1)
+  }
+}
+function handleUpdateTask(task) {
+  const index = tasks.value.findIndex(t => t.code === task.code)
+  tasks.value.splice(index,1,task);
+}
+
+function showDelete(itemCode) {
+    hoveredItem.value = itemCode;
+}
+function hideDelete() {
+    hoveredItem.value = null;
+}
+
 </script>
