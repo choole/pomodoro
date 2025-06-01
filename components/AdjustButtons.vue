1b43c5613a1ae85c5825c32f48995fc2497d4108
Punschkrapferl <waldsa01@thu.de>
2025-06-01 22:55:54 +0200

    Add: fullscreen, settings button

--- components/AdjustButtons.vue
+++ components/AdjustButtons.vue
@@ -3,6 +3,7 @@
       <template #center>
          <div class="flex justify-center space-x-4">
             <Button icon="pi pi-replay" severity="secondary" variant="text" raised rounded aria-label="Replay"/>
+			<!-- FIXME:  disable play after start, enable after pause -->	
             <Button icon="pi pi-play-circle" severity="secondary" variant="text" raised rounded aria-label="Play" @click="timerStore.startTimer(tab)"/>
             <Button icon="pi pi-pause-circle" severity="secondary" variant="text" raised rounded aria-label="Pause " @click="timerStore.pauseTimer(tab)"/>
          </div>
@@ -15,6 +16,7 @@ import Button from 'primevue/button';
 import Toolbar from 'primevue/toolbar';
 import 'primeicons/primeicons.css';
 import { eventBus} from '@/utils/event-bus';
+
 
 const tab=ref(0);
 
--- components/MainComponent.vue
+++ components/MainComponent.vue
@@ -1,26 +1,52 @@
 <template>
-    <div class="card">
-        <Splitter style="height: 100vh">
-            <SplitterPanel class="flex flex-col items-center justify-center"> 
-                <div class="pb-10">INSERT LOGO HERE</div>
-                <!-- <ComponentName/> -->
-						<HeaderTabs/>
-						<!-- <TimerInput/> -->
-						<AdjustButtons/>
-            </SplitterPanel>
-            <SplitterPanel class="flex flex-col justify-center">
-                <div class="mt-10 text-3xl text-center">Tasks</div>
-                <TaskList/>
-            </SplitterPanel>
-        </Splitter>
-    </div>
+  <div class="card">
+    <Splitter style="height: 100vh">
+      
+      <SplitterPanel>
+       <div class="flex justify-end w-full px-4 mt-4">
+          <Button 
+            icon="pi pi-external-link" 
+            severity="secondary"
+            variant="text" 
+            rounded 
+            aria-label="Fullscreen"
+          />
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <!-- <ComponentName/> -->
+          <HeaderTabs />
+          <!-- <TimerInput/> -->
+          <AdjustButtons />
+        </div>
+      </SplitterPanel>
+
+      <SplitterPanel>
+		<div class="flex justify-end w-full px-4 mt-4">
+          <Button 
+            icon="pi pi-cog" 
+            severity="secondary"
+            variant="text" 
+            rounded 
+            aria-label="Settings"
+          />
+        </div>
+
+	  <div class="flex flex-col justify-center">
+          <div class="mt-10 text-3xl text-center">Tasks</div>
+          <TaskList />
+        </div>
+      </SplitterPanel>
+
+    </Splitter>
+  </div>
 </template>
 
-
-  <script setup lang="ts">
+<script setup lang="ts">
 import Splitter from 'primevue/splitter';
 import SplitterPanel from 'primevue/splitterpanel';
 import HeaderTabs from './HeaderTabs.vue';
 import TaskList from './TaskList.vue';
 import AdjustButtons from './AdjustButtons.vue';
-  </script>
+import Button from 'primevue/button';
+</script>
